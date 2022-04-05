2eec1c5457a0cfb6379bfc661a25a4681093c66f
victorwu <victorwu@users.noreply.github.com>
2022-04-05 10:03:34 -0400

    feature: use proxy for CORS

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:3344/",
+  // baseURL: "http://localhost:3344/",
   // baseURL: "https://tokenlists.herokuapp.com",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://tokenlists.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
